e6842ef6c9868df73504f1e19cd5e98d1f5b9de1
Andres Vargas <a.m.vargas1996@gmail.com>
2023-05-06 19:58:01 -0300

    Añade labels dinamicos a la barra de busqueda

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,24 +1,68 @@
 'use client';
 
+import useCountries from '@/app/hooks/useCountries';
 import useSearchModal from '@/app/hooks/useSearchModal';
+import { differenceInDays } from 'date-fns';
+import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
 import { BiSearch } from 'react-icons/bi';
 
 const Search = () => {
 
   const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const  { getByValue } = useCountries();
 
+  // Obtiene valores de parametros en la url
+  const locationValue = params?.get("locationValue");
+  const startDate = params?.get("startDatee");
+  const endDate = params?.get("endDate");
+  const guestCount = params?.get("guestCount");
+
+
+  /* Cambia los valores de los label en la barra de busqueda segun los valores antes definidos */
+  const locationLabel = useMemo(() => {
+    
+    if(locationValue) return getByValue(locationValue)?.label;
+
+    return 'Lugar'
+
+  }, [getByValue, locationValue])
+
+  const durationLabel = useMemo(() => {
+    if(startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if(diff === 0) diff = 1;
+
+      return `${diff} días`
+    }
+
+    return "Duración"
+
+  },[startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if(guestCount) return `${guestCount} personas`;
+
+    return "Huéspedes"
+  }, [guestCount]);
+
+  
   return (
     <div onClick={searchModal.onOpen} className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
       <div className="flex flex-row items-center justify-between">
         <div className="text-sm font-semibold px-6">
-          Lugar
+          {locationLabel}
         </div>
         <div className="sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-          Fecha
+          {durationLabel}
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
           <div className="sm:block">
-            Huéspedes
+              {guestLabel}
           </div>
           <div className="p-2 bg-rose-500 rounded-full text-white">
             <BiSearch size={18} />
